c1d80a8fe30910e2c3fa03b4df20a56733b0cad8
EAGLEEARTH <ufukcicek199@gmail.com>
2023-03-12 00:47:46 +0300

    schedule added

--- main.py
+++ main.py
@@ -5,6 +5,8 @@ from dotenv import load_dotenv
 import os
 import logging
 from datetime import datetime
+import time
+import schedule
 
 logging.basicConfig(filename='logFile.log', encoding='utf-8', level=logging.DEBUG)
 
@@ -20,17 +22,12 @@ subject = 'Python Email Sender (Test Mail)'
 body = """
 Test Mail 
 """
-
-
 def mailMessage():
+    em = EmailMessage()
     em["From"] = email_sender
     em["To"] = email_receiver
     em["Subject"] = subject
     em.set_content(body)
-
-
-
-
 
 def attachmentReader():
     for fileName in os.listdir(attachment_path):
@@ -42,18 +39,30 @@ def attachmentReader():
 def mailSender():
     try:
         context = ssl.create_default_context()
-        with smtplib.SMTP_SSL('smtp.gmail.com',465, context=context) as smtp:
-            smtp.login(email_sender,email_password)
-            smtp.sendmail(email_sender, email_receiver, em.as_string())
+        with smtplib.SMTP_SSL('smtp.gmail.com',465,context=context) as server:
+            server.login(email_sender,email_password)
+            server.sendmail(email_sender, email_receiver, em.as_string())
             logging.info(f"Email Sent Done! -- Time: {datetime.now()} -- Receiver: {email_receiver}")
-
+            server.quit()
     except Exception as e:
         logging.error("Exception occurred", exc_info=True)
 
 
 
-if __name__ == '__main__':
+def job():
     mailMessage()
     attachmentReader()
-    mailSender()
+    mailSender()   
 
+schedule.every(10).seconds.do(job)
+# schedule.every(10).minutes.do(job)
+# schedule.every().hour.do(job)
+# schedule.every().day.at("10:30").do(job)
+# schedule.every(5).to(10).minutes.do(job)
+# schedule.every().monday.do(job)
+# schedule.every().wednesday.at("13:15").do(job)
+# schedule.every().minute.at(":17").do(job)
+
+while True:
+    schedule.run_pending()
+    time.sleep(1)
